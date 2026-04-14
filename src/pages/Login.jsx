305beebf2619bc1bf3fdd6833955b199ea59d741
ARNOLD0327 <arnoldjohnson652@gmail.com>
2026-04-14 19:45:06 +0530

    error solved

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import "./Login.css";
+import "./login.css";
 import { useNavigate } from "react-router-dom";
 function Login() {
   const [formData, setFormData] = useState({
